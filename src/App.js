c6cf96d3672e0a4a7e11088b59fceb1e52fdf85b
taitonabeshima <taito.nabeshima@gmail.com>
2022-07-24 21:17:22 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState, useRef } from "react";
+import TodoList from './TodoList';
+import {v4 as uuidv4 } from "uuid";
+
 import './App.css';
 
 function App() {
+const [todos, setTodos] = useState([]);
+
+  const todoNameRef = useRef();
+
+
+  const handleAddTodo = () => {
+    //タスクを追加する
+    // console.log(todoNameRef.current.value);
+    const name = todoNameRef.current.value;
+    if(name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false }];
+      //前のタスクに対して、新しいタスクを追加していく
+    } );
+    todoNameRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos =[...todos];
+    const todo =newTodos.find((todo)=> todo.id===id);
+    todo.completed =!todo.completed;
+    setTodos(newTodos);
+
+  };
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//returnの中には大きなdivタグがないといけない
+    <div>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク：{todos.filter((todo) => !todo.completed).length}</div>
+
     </div>
+
   );
 }
 
